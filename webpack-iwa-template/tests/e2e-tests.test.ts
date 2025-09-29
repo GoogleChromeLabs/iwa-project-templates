41e3d03848467d0671b27285ab9de4e3c059696e
Michal Borowski <borowskimichal@google.com>
2025-09-29 13:47:54 +0000

    Prettier formatting

--- webpack-iwa-template/tests/e2e-tests.test.ts
+++ webpack-iwa-template/tests/e2e-tests.test.ts
@@ -1,7 +1,7 @@
-import * as puppeteer from 'puppeteer-core';
-import * as path from 'path';
-import * as wbnSign from 'wbn-sign';
-import * as fs from 'fs';
+import * as puppeteer from "puppeteer-core";
+import * as path from "path";
+import * as wbnSign from "wbn-sign";
+import * as fs from "fs";
 
 /*
   If you want to debug E2E tests, you will need to either create a custom debug launch configuration
@@ -10,37 +10,35 @@ import * as fs from 'fs';
   Alternatively, you can use a test runner extension. 
   https://marketplace.visualstudio.com/items?itemName=firsttris.vscode-jest-runner
 */
-describe('E2E Tests', () => {
-  let browser: puppeteer.Browser;
-  let session: puppeteer.CDPSession;
-  let manifestID: string;
+describe("E2E Tests", () => {
+    let browser: puppeteer.Browser;
+    let session: puppeteer.CDPSession;
+    let manifestID: string;
 
-  const bundlePath = path.join(process.cwd(), 'dist', 'iwa-template.swbn');
-  const flags = "--enable-features=IsolatedWebApps,IsolatedWebAppDevMode";
-  test("IWA Should be installed and launched correctly",async ()   => {
-    const bundleContent = new Uint8Array(await fs.promises.readFile(bundlePath));
-    manifestID = wbnSign.getBundleId(bundleContent);
+    const bundlePath = path.join(process.cwd(), "dist", "iwa-template.swbn");
+    const flags = "--enable-features=IsolatedWebApps,IsolatedWebAppDevMode";
+    test("IWA Should be installed and launched correctly", async () => {
+        const bundleContent = new Uint8Array(await fs.promises.readFile(bundlePath));
+        manifestID = wbnSign.getBundleId(bundleContent);
 
-    browser = await puppeteer.launch({
-      executablePath: '/usr/bin/google-chrome',
-      headless: false,
-      pipe: true,
-      args: [flags]
-    });
+        browser = await puppeteer.launch({
+            executablePath: "/usr/bin/google-chrome",
+            headless: false,
+            pipe: true,
+            args: [flags],
+        });
 
-    session = await browser.target().createCDPSession();
+        session = await browser.target().createCDPSession();
 
-    await session.send("PWA.install", {
-      manifestId: `isolated-app://${manifestID}`,
-      installUrlOrBundleUrl: `file://${bundlePath}`
-    });
+        await session.send("PWA.install", {
+            manifestId: `isolated-app://${manifestID}`,
+            installUrlOrBundleUrl: `file://${bundlePath}`,
+        });
 
-    await session.send("PWA.launch", {
-      manifestId: `isolated-app://${manifestID}`
-    })
+        await session.send("PWA.launch", {
+            manifestId: `isolated-app://${manifestID}`,
+        });
 
-    await browser.close();
-
-  }, 300000 );
-
+        await browser.close();
+    }, 300000);
 });
